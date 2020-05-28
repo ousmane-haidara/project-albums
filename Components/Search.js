1b172d6226d37f1ae0113727a60f4925cba4ee2c
HAIDARA <ousmanehaidara26@gmail.com>
2020-05-28 15:14:27 +0200

    externisation des styles

--- Components/Search.js
+++ Components/Search.js
@@ -6,13 +6,29 @@ import { View, TextInput, Button } from 'react-native';
 const Search = () => {
     return (
 
-        <View style = {{ marginTop: 40}} >
-            <TextInput placeholder='Titre du film'/>
+        <View style = {{ marginTop: 45 }} >
+            <TextInput style = {styles.textinput} placeholder='Titre du film'/>
             <Button title='Rechercher' onPress={() => {}}/>
         </View>
 
     )
 }
+
+
+//Externalisation des styles !!
+
+const styles = { // constante contenant toutes nos styles react native !!!
+
+    textinput: {
+      marginLeft: 10,
+      marginRight: 10,
+      height: 50,
+      borderColor: '#000000',
+      borderWidth: 1,
+      paddingLeft: 5
+    }
+
+  }
 
 
 
