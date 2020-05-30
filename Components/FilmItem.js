b0aa77d695926ff0b286d23b8ea0984d15004db6
HAIDARA <ousmanehaidara26@gmail.com>
2020-05-30 18:33:50 +0200

    utilisation des props pour afficher les données du film

--- Components/FilmItem.js
+++ Components/FilmItem.js
@@ -5,20 +5,21 @@ import { StyleSheet, View, Text, Image } from 'react-native'
 
 class FilmItem extends React.Component {
   render() {
+    const film = this.props.film;
     return (
       <View style={styles.main_container}>
         <Image style={styles.image} source={{uri: "image"}}/>
         <View style={styles.content_container}>
           <View style={styles.header_container}>
-            <Text style={styles.title_text}>Titre du film</Text>
-            <Text style={styles.vote_text}>Vote</Text>
+    <Text style={styles.title_text}>{film.title}</Text>
+            <Text style={styles.vote_text}>{film.vote_average}</Text>
           </View>
           <View style={styles.description_container}>
-            <Text style={styles.description_text} numberOfLines={6}>Description</Text>
+            <Text style={styles.description_text} numberOfLines={6}>{film.overview}</Text>
             {/* La propriété numberOfLines permet de couper un texte si celui-ci est trop long, il suffit de définir un nombre maximum de ligne */}
           </View>
           <View style={styles.date_container}>
-            <Text style={styles.date_text}>Sorti le 00/00/0000</Text>
+            <Text style={styles.date_text}>Sorti le {film.release_date}</Text>
           </View>
         </View>
       </View>
@@ -49,7 +50,7 @@ const styles = StyleSheet.create({
     fontWeight: 'bold',
     fontSize: 20,
     flex: 1,
-    flexWrap: 'wrap',
+    flexWrap: 'wrap', //le style  flexWrap: 'wrap'  sur le titre du film qui permet à notre texte de passer à la ligne si celui-ci est trop long
     paddingRight: 5
   },
   vote_text: {
