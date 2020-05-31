56213c2bc059aa262401056a4b5f8dab2e5d06ce
HAIDARA <ousmanehaidara26@gmail.com>
2020-05-31 23:28:53 +0200

    Utilisation des states

--- Components/Search.js
+++ Components/Search.js
@@ -1,40 +1,46 @@
 // Components/Search.js
 
-import React from 'react';
-import { StyleSheet, View, TextInput, Button, FlatList} from 'react-native';
-import FilmItem from './FilmItem';
-//import films from '../Helpers/filmsData';
-import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi';
+import React from 'react'
+import { StyleSheet, View, TextInput, Button, Text, FlatList } from 'react-native'
+import FilmItem from './FilmItem'
+import { getFilmsFromApiWithSearchedText } from '../API/TMDBApi'
 
 class Search extends React.Component {
 
   constructor(props) {
-
-    super(props);
-    this._films = [];
-
+    super(props)
+    this.searchedText = "" // Initialisation de notre donnée searchedText en dehors du state
+    this.state = {
+      films: []
+    }
   }
 
-
-
   _loadFilms() {
+    if (this.searchedText.length > 0) { // Seulement si le texte recherché n'est pas vide
+      getFilmsFromApiWithSearchedText(this.searchedText).then(data => {
+          this.setState({ films: data.results })
+      })
+    }
+  }
 
-    getFilmsFromApiWithSearchedText("star").then(data => {
-      this._films = data.results;
-      this.forceUpdate();
-    });
-
+  _searchTextInputChanged(text) {
+    this.searchedText = text // Modification du texte recherché à chaque saisie de texte, sans passer par le setState comme avant
   }
 
   render() {
+    console.log("RENDER")
     return (
       <View style={styles.main_container}>
-        <TextInput style={styles.textinput} placeholder='Titre du film'/>
-        <Button title='Rechercher' onPress={ () => this._loadFilms() }/>
+        <TextInput
+          style={styles.textinput}
+          placeholder='Titre du film'
+          onChangeText={(text) => this._searchTextInputChanged(text)}
+        />
+        <Button title='Rechercher' onPress={() => this._loadFilms()}/>
         <FlatList
-          data={this._films}
+          data={this.state.films}
           keyExtractor={(item) => item.id.toString()}
-          renderItem={({item}) => <FilmItem film={ item }/>}
+          renderItem={({item}) => <FilmItem film={item}/>}
         />
       </View>
     )
