65a4717a1bb5c69d266fa2613d6b9cd4eab27af9
HAIDARA <ousmanehaidara26@gmail.com>
2020-06-01 15:43:43 +0200

    ajout de props onSubmitEditing pour lancer la recherche depuis le clavier

--- Components/Search.js
+++ Components/Search.js
@@ -35,6 +35,7 @@ class Search extends React.Component {
           style={styles.textinput}
           placeholder='Titre du film'
           onChangeText={(text) => this._searchTextInputChanged(text)}
+          onSubmitEditing={() => this._loadFilms()}
         />
         <Button title='Rechercher' onPress={() => this._loadFilms()}/>
         <FlatList
